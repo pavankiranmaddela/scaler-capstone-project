4e72d080098dc458d77837d730e3040c8355d592
pavan kiran maddela <33048163+pavankiranmaddela@users.noreply.github.com>
2026-04-02 12:53:46 +0530

    VC-1 : Initial Commit.

---
+++ vehicle-catalog-service/src/main/java/com/pk/ev/vehicle/catalog/VehicleCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pk.ev.vehicle.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ vehicle-catalog-service/src/test/java/com/pk/ev/vehicle/catalog/VehicleCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.ev.vehicle.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
